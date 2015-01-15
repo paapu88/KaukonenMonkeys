a2c392705a1b23a58c3d9e57f74d43b568c7beb5
Markus Kaukonen <markus.kaukonen@iki.fi>
2015-01-15 16:40:18 +0200

    config.py updated for heroku app

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ PASSWORD='default'
 DEBUG = False
 SECRET_KEY = 'Laa12Kis3'
 
-SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(basedir, 'app.db') +
-                           '?check_same_thread=False')
+#SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(basedir, 'app.db') +
+#                           '?check_same_thread=False')
 
